7bb8017ce193b51fa94cd6c8811676b92400814d
Carolina <carolinamartinsfreitas@gmail.com>
2021-05-16 20:31:28 -0300

    Adicionando style na autenticação

--- src/Auth.js
+++ src/Auth.js
@@ -1,43 +1,41 @@
 import React, { useEffect, useState } from 'react';
-import Loading from './pages/Loading'
-import SignIn from './pages/SignIn'
-import App from './pages/App'
-import ErrorPage from './pages/ErrorPage'
-import { checkToken, getStorageToken } from './services/AuthService';
+import Loading from './pages/Loading';
+import SignIn from './pages/SignIn';
+import App from './pages/App';
+import ErrorPage from './pages/ErrorPage';
+import { getStorageToken } from './services/AuthService';
 
 export default function Auth() {
-  const [component, setComponent] = useState('Loading')
-  const [error, setError] = useState('')
+  const [component, setComponent] = useState('Loading');
+  const [error, setError] = useState('');
 
   useEffect(() => {
     getStorageToken().then(
-      token => {
-        if(token !== null){
-          setComponent('App')
+      (token) => {
+        if (token !== null) {
+          setComponent('App');
+        } else {
+          console.log(token);
+          setComponent('SignIn');
         }
-        else{
-          console.log(token)
-          setComponent('SignIn')
-        }
-      }
-    ).catch(err => {
-      console.log(err)
-      setComponent('ErrorPage')
-      setError('getStorageToken')
-    })
-  }, [])
+      },
+    ).catch((err) => {
+      console.log(err);
+      setComponent('ErrorPage');
+      setError('getStorageToken');
+    });
+  }, []);
 
-
-  if(component === 'Loading'){
-    return <Loading/>
+  if (component === 'Loading') {
+    return <Loading />;
   }
-  else if(component === 'SignIn'){
-    return <SignIn error={error}/>
+  if (component === 'SignIn') {
+    return <SignIn error={error} />;
   }
-  else if(component === 'App'){
-    return <App/>
+  if (component === 'App') {
+    return <App />;
   }
-  else if(component === 'ErrorPage'){
-    return <ErrorPage error={error}/>
+  if (component === 'ErrorPage') {
+    return <ErrorPage error={error} />;
   }
 }
